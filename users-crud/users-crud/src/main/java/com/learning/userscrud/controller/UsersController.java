480702a2d2bea21b1e2c523521d370edb50c9c66
teperm <marina.teper@dell.com>
2022-11-07 14:23:25 +0200

    test

--- users-crud/users-crud/src/main/java/com/learning/userscrud/controller/UsersController.java
+++ users-crud/users-crud/src/main/java/com/learning/userscrud/controller/UsersController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/users")
+@RequestMapping("/${users_controller.path}")//"/api/v1/users")
 public class UsersController {
 
     @Autowired
